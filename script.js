7364675252d13c69372917df8623fbf95880505e
RobbieGodschalk <r.godschalk@student.tue.nl>
2021-04-25 17:01:51 +0200

    Loading from CSV
    Using D3, I made a VERY simple function to load the data, convert the needed values, and log it to the console. Although not yet very functional, we can at least get the data.
    
    Currently facing the problem of how to access specific data from the CSV (a certain line/column). This may require a different approach than currently used.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+//Returns a "promise" : object representing the eventual completion (or failure) of an asynchronous operation and its
+// resulting value.
+
+//NOTE: CHROME ONLY ALLOWS LOADING OF CSV FROM SERVER / WITH SPECIAL ACCESS. TESTING IS EASIEST
+// VIA FIREFOX OR ANOTHER BROWSER, BUT THIS IS SOMETHING TO KEEP IN MIND.
+testData = d3.csv("./enron-v1.csv", function(d) {
+
+    return {
+        date: new Date(d.date), //Convert date to Date
+        fromEmail: d.fromEmail,
+        fromId: +d.fromId,
+        fromJobtitle: d.fromJobtitle,
+        messageType: d.messageType,
+        sentiment: +d.sentiment,
+        toEmail: d.toEmail,
+        toId: +d.toId,
+        toJobtitle: d.toJobtitle
+    };
+});
+
+console.log(testData);
