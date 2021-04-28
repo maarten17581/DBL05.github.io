4e6b519ecf60de1b90118c54d514a4665a4dea94
maarten17581 <m.dankers@student.tue.nl>
2021-04-28 14:59:26 +0200

    Filter added and upload added

--- functional.js
+++ functional.js
@@ -38,17 +38,90 @@ function addFrame() {
     amountOfFrames++;
 }
 
-function removeFrame(id) {
+function removeFrame(id) { // delete an visualisation div
 
     document.getElementById(id).parentNode.removeChild(document.getElementById(id));
 }
 
-function makeVisualisation(id, value) {
+function makeVisualisation(id, value) { // set div to specific visualisation
     document.getElementById(id+"middle").parentNode.removeChild(document.getElementById(id+"middle"));
 
     const div = document.createElement("div");
 
-    div.className="middle";
     div.innerHTML=`The visualisation is `+value;
     document.getElementById(id+"visual").appendChild(div);
+}
+
+var filterAmount = 0;
+
+var columns = ['FromId', 'ToId', 'Date', 'Sentiment'];
+
+function setColumns(column) // set columns of the data for the filters
+{
+    columns = column;
+    document.getElementById('selection').innerHTML='';
+    makeNewFilter();
+}
+
+function makeNewFilter() { // set an extra filter
+
+    const div = document.createElement("div");
+
+    div.id=filterAmount+"globalSelectDiv";
+    var innerHTML =`
+    <select name="`+filterAmount+`globalSelect" id="`+filterAmount+`globalSelect">
+    `;
+
+    var selected = `selected`;
+    for(var i = 0; i < columns.length; i++)
+    {
+        innerHTML += `<option `+selected+` value='`+columns[i]+`'> `+columns[i]+` </option>`;
+        selected = ``;
+    }
+
+    innerHTML += `</select>
+    <select name="`+filterAmount+`globalSign" id="`+filterAmount+`globalSign">
+        <option selected value=&lt> &lt; </option> <!--the sign-->
+        <option value='gt'> &gt; </option>
+        <option value='e'> = </option>
+        <option value='ne'> &ne; </option>
+        <option value='leq'> &leq; </option>
+        <option value='geq'> &geq; </option>
+    </select>
+    <input type="number" name="value" id="`+filterAmount+`value" value="0" style="width: 75%;">
+    <input type="button" name="removeFilter" id="`+filterAmount+`removeFilter" value="x" onclick="removeFilter(`+filterAmount+`)"><br>
+    `;
+
+    div.innerHTML = innerHTML;
+
+    document.getElementById("selection").appendChild(div);
+
+    filterAmount++;
+}
+
+function removeFilter(id) {
+    document.getElementById('selection').removeChild(document.getElementById(id+'globalSelectDiv'));
+}
+
+function getGlobalFilter() { // get info from filters
+
+    var field = [];
+    var sign = [];
+    var value = [];
+
+    for(var i = 0; i <= filterAmount; i++)
+    {
+        field.push(document.getElementById(filterAmount+"globalSelect").value);
+        sign.push(document.getElementById(filterAmount+"globalSign").value);
+        value.push(document.getElementById(filterAmount+"value").value);
+    }
+}
+
+function uploadData()
+{
+    const file = document.getElementById("data").files;
+    if(file.substring(file.length-4, file.length-1) == "csv")
+    {
+        
+    }
 }
